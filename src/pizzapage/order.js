4ca97aa1e252949133d8d826f066aa9142c1054e
zchips <zchips@gmail.com>
2023-05-25 11:05:49 -0600

    finishing out orders page

---
+++ src/pizzapage/order.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+
+function Order(props){
+    
+    
+    return(
+        <div>
+                    <Order>
+                        <h1> Order Here!</h1> 
+                {/* title and then label for the following form with text boxes */}
+                                <label>
+                    Order For:  <input name="Name" id="name-input" type="text" value={values.name}><br></br></input>
+                                </label>
+                                
+                                <label>
+                    Pizza Size:     <select name="size" id="size" value={values.size} >
+                                    <option value=''> Choose your size pizza!</option>
+                                    <option value="small"> small 12inch</option>
+                                    <option value="medium"> medium 16inch</option>
+                                    <option value="large"> large 20inch</option>
+                                    </select>                
+                                </label>
+
+
+                                <h2>Toppings</h2>
+                                <label>
+                                    Bacon <input type="checkbox" name="Bacon" checked={values.bacon} onChange={onChange}/>
+                                </label>
+                                <label>
+                                    Sausage <input type="checkbox" name="Sausage" checked={values.sausage} onChange={onChange}/>
+                                </label>
+                                <label>
+                                   Pepperoni <input type="checkbox" name="Pepperoni" checked={values.pepperoni} onChange={onChange}/>
+                                </label>
+                                <label>
+                                  Peppers  <input type="checkbox" name="Peppers" checked={values.peppers} onChange={onChange}/>
+                                </label>
+                                <label>
+                                   Mushrooms <input type="checkbox" name="Mushrooms" checked={values.mushrooms} onChange={onChange}/>
+                                </label>
+                                <label>
+                                    Pineapple<input type="checkbox" name="Pineapple" checked={values.pineapple} onChange={onChange}/>
+                                </label>
+                                <label>
+                                   Allergies? <input type="text" name="Custom" checked={values.custom} onChange={onChange} id="custom-box" placeholder="Let us know here"/>
+                                </label>
+                                
+
+                        
+                        <div className="errors">
+                            <span>{errors.name}</span>
+                            <span>{errors.size}</span>
+                        </div>
+
+                                    <button id="order-button" disabled={disabled}>Submit</button>
+                    </Order>
+            
+        </div>
+    )
+}
+export default Order
