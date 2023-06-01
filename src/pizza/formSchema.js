b2b29e5e7321e5a2d4c6b9e83d2c81a63283edb4
zchips <zchips@gmail.com>
2023-06-01 16:08:11 -0600

    codegrade push 2

--- src/pizza/formSchema.js
+++ src/pizza/formSchema.js
@@ -1,15 +1,15 @@
 import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
-    name: yup.string().trim().min(2,"name must be at least 2 characters"),
-    // custom: yup.string(),
-    // size: yup.string(),
-    // bacon: yup.boolean(),
-    // sausage: yup.boolean(),
-    // pepperoni: yup.boolean(),
-    // peppers: yup.boolean(),
-    // mushrooms: yup.boolean(),
-    // pineapple: yup.boolean(),
+    name: yup.string().trim().required("Name is required").min(2, "Name must be at least 2 characters"),
+    custom: yup.string(),
+    size: yup.string().oneOf(['small', 'medium', 'large'], 'Please select a valid size'),
+    bacon: yup.boolean(),
+    sausage: yup.boolean(),
+    pepperoni: yup.boolean(),
+    peppers: yup.boolean(),
+    mushrooms: yup.boolean(),
+    pineapple: yup.boolean(),
 })
 
 export default formSchema
