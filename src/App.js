899609988b2e571131998b3d6855187ccd970e07
zchips <zchips@gmail.com>
2023-06-25 13:40:10 -0600

    wip 4?

--- src/App.js
+++ src/App.js
@@ -1,77 +1,83 @@
 import React, { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 import { Route, Link, Routes } from "react-router-dom";
 import Order from "./pizza";
-import Confirmation from "./pizza/Confirmation";
 import formSchema from "./pizza/formSchema";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import * as yup from 'yup';
+import axios from "axios";
+import * as yup from "yup";
 import HomePage from "./HomePage";
 
-
-const blankOrder = []
-const initialDisabled = true;
 const initialFormErrors = {
-  name: '',
-  size: '',
-  custom: ''
-}
-
+  name: "",
+  size: "",
+  custom: "",
+};
 
 const initialFormValues = {
-  name: '',
-  size:'',
-  custom: '',
+  name: "",
+  size: "",
+  custom: "",
   bacon: false,
   sausage: false,
   pepperoni: false,
   peppers: false,
   mushrooms: false,
   pineapple: false,
-}
-
+};
 
 const App = () => {
-  const navigate = useNavigate();
-  const [order, setOrder] = useState(blankOrder);
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(initialDisabled);
+  const [disabled, setDisabled] = useState(true);
 
-  const confirmation = () => {
-    navigate('/pizza/confirmation');
-  }
-  const validate = (name, value) =>{
-    yup.reach(formSchema, name).validate(value).then(()=> setFormErrors({...formErrors, [name]:''})).catch(error => setFormErrors({...formErrors, [name]: error.errors[0]}));
-  }
-  
+  const validate = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() =>
+        setFormErrors((prevErrors) => ({ ...prevErrors, [name]: "" }))
+      )
+      .catch((error) =>
+        setFormErrors((prevErrors) => ({ ...prevErrors, [name]: error.errors[0] }))
+      );
+  };
 
-  const postOrder = newOrder => {
-    axios.post(`https://reqres.in/api/orders`, newOrder).then(res => {setOrder([res.data, ...order])}).catch(error=>{console.log(error)}).finally(()=>{
-      setFormValues(initialFormValues);
-      confirmation();
-    });
-  }
+  const postOrder = (newOrder) => {
+    axios
+      .post(`https://reqres.in/api/orders`, newOrder)
+      .then((res) => {
+        console.log("Order submitted:", res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const inputChange = (name, value) => {
     validate(name, value);
-    setFormErrors({...formValues, [name]: value});
-  }
+    setFormValues((prevFormValues) => ({ ...prevFormValues, [name]: value }));
+  };
 
   const formSubmit = () => {
     const newOrder = {
       name: formValues.name.trim(),
       size: formValues.size,
       custom: formValues.custom,
-      toppings: ['bacon', 'sausage', 'pepperoni', 'peppers', 'mushrooms', 'pinneapple']
-      .filter(toppings =>formValues[toppings])
-    }
+      toppings: [
+        "bacon",
+        "sausage",
+        "pepperoni",
+        "peppers",
+        "mushrooms",
+        "pinneapple",
+      ].filter((toppings) => formValues[toppings]),
+    };
     postOrder(newOrder);
-  }
+    setFormValues(initialFormValues);
+  };
 
-  useEffect(()=> {
-    formSchema.isValid(formValues).then(valid => setDisabled(!valid));
+  useEffect(() => {
+    formSchema.isValid(formValues).then((valid) => setDisabled(!valid));
   }, [formValues]);
 
   return (
@@ -79,17 +85,29 @@ const App = () => {
       <h1>Lambda Za</h1>
       <p>You can remove this code and create your own header or I can not</p>
       <div className="nav-links">
-        <Link to='/'> Home</Link>
-        <Link to='/pizza' id='pizza-order'> Order</Link>
+        <Link to="/"> Home</Link>
+        <Link to="/pizza" id="pizza-order">
+          Order
+        </Link>
       </div>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/pizza/confirmation" element={<Confirmation />} />
-        <Route path="/pizza" element={<Order values={formValues} disabled={disabled} change={inputChange} submit={formSubmit} errors={formErrors} />} />
+        <Route
+          path="/pizza"
+          element={
+            <Order
+              values={formValues}
+              disabled={disabled}
+              change={inputChange}
+              submit={formSubmit}
+              errors={formErrors}
+            />
+          }
+        />
       </Routes>
     </div>
   );
 };
 
-export default App
+export default App;
--- src/pizza/Confirmation.js
+++ src/pizza/Confirmation.js
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-function Confirmation(props){
-    const {pizza} = props
-    return(
-        <div>
-            <h1>Confirmation</h1>
-            <h2>thank you {pizza.name} for ordering a {pizza.size} pizza with our famous dough and your choice of:</h2>
-            <ul>{pizza.Toppings.map((topping, idx) => <li key={idx}>{topping}</li>)}</ul>
-            <h2> customizations (if requested) {pizza.custom}</h2>
-        </div>
-    )
-}
+// function Confirmation(props){
+//     const {pizza} = props
+//     return(
+//         <div>
+//             <h1>Confirmation</h1>
+//             <h2>thank you {pizza.name} for ordering a {pizza.size} pizza with our famous dough and your choice of:</h2>
+//             <ul>{pizza.Toppings.map((topping, idx) => <li key={idx}>{topping}</li>)}</ul>
+//             <h2> customizations (if requested) {pizza.custom}</h2>
+//         </div>
+//     )
+// }
 
-export default Confirmation
+// export default Confirmation
