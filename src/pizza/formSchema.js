31f9a25de6a30df64e8cd2abcdd7201061c3cb24
zchips <zchips@gmail.com>
2023-06-27 09:50:14 -0600

    wip 4 formschema corrections

--- src/pizza/formSchema.js
+++ src/pizza/formSchema.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
-    name: yup.string().trim().required("Name is required").min(2, "Name must be at least 2 characters"),
+    name: yup.string().trim().min(2, "Name must be at least 2 characters"),
     custom: yup.string(),
     size: yup.string().oneOf(['small', 'medium', 'large'], 'Please select a valid size'),
     bacon: yup.boolean(),
